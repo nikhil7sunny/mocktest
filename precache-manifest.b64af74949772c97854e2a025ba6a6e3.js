9ea512dec60edf8c55107e1c3aa39f1db2f75484
nikhil <nikhil7sunny@cet.ac.in>
2020-05-05 00:12:32 +0530

    Updates

---
+++ precache-manifest.b64af74949772c97854e2a025ba6a6e3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b36935a07311f7f9d558654112e475fb",
+    "url": "/mocktest/index.html"
+  },
+  {
+    "revision": "9f123fd225c0cfbd4628",
+    "url": "/mocktest/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "5c98a67ee832dc638d4b",
+    "url": "/mocktest/static/css/main.b9673cbe.chunk.css"
+  },
+  {
+    "revision": "9f123fd225c0cfbd4628",
+    "url": "/mocktest/static/js/2.651af5d2.chunk.js"
+  },
+  {
+    "revision": "7585d7e2191ead1c7fd01d7ebe90ade7",
+    "url": "/mocktest/static/js/2.651af5d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c98a67ee832dc638d4b",
+    "url": "/mocktest/static/js/main.a83013bf.chunk.js"
+  },
+  {
+    "revision": "d4c725bf79eef8a4b694",
+    "url": "/mocktest/static/js/runtime-main.1abb96a9.js"
+  },
+  {
+    "revision": "40a1bb0a0a5feb4fbc47fa0cc59962c6",
+    "url": "/mocktest/static/media/locus logo.40a1bb0a.jpg"
+  }
+]);
